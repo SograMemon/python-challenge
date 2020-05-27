8f1efc4cd9ba9ba36d226be3ed3dcae947c0f72f
Sogra Memon <sogra.memon@dal.ca>
2020-05-27 07:33:41 -0400

    Partly working code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,5 +7,15 @@ with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     myheaders= next(csvreader)
     print(f"My Headers: {myheaders}")
+    voteN=0
     for row in csvreader:
-        print(row)
+        voteN += 1
+        #print(row)
+    
+    print("Election Results")
+    print("-------------------------")
+    print(f"Total Votes: {voteN}")
+    print("-------------------------")
+    print("-------------------------")
+    print("-------------------------")
+    
