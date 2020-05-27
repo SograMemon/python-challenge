69ef89ee329e29847ebd14e7331cf6658b2065f5
Sogra Memon <sogra.memon@dal.ca>
2020-05-27 18:19:55 -0400

    update error corrections

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,6 +6,25 @@ csvpath= os.path.join('Resources','budget_data.csv')
 with open (csvpath) as file_handler:
     lines = csv.reader(file_handler, delimiter=',')
     myvariable=next(lines)
-    print(f"These are my csv headrs: {myvariable}")
+    #print(f"These are my csv headrs: {myvariable}")
+    
+    greatestInc =greatestDec = next(lines)
+    monthsN= 1
+    total= int(greatestInc[1])
     for row in lines:
-        print(row)
+        monthsN += 1
+        total += int(row[1])
+        if greatestInc[1] < row[1]:
+            greatetInc = row
+        if greatestDec[1] > row[1]:
+            greatestDec = row
+
+        #print(row)
+    avg= total/monthsN
+    print('Financial Analysis')
+    print('----------------------------')
+    print (f"Total Months: {monthsN}")
+    print (f"Total: $ {total}")
+    print(f"Average Change: $ {avg}")
+    print(f"Greates Increase in profits: {greatestInc[0]} ($ {greatestInc[1]})")
+    print(f"Greates Decrease in profits: {greatestDec[0]} ($ {greatestDec[1]})")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,16 +6,45 @@ csvpath = os.path.join("Resources","election_data.csv")
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     myheaders= next(csvreader)
-    print(f"My Headers: {myheaders}")
-    voteN=0
+    #print(f"My Headers: {myheaders}")
+    voteN=1
+    rowOne= next(csvreader)
+    #print(rowOne)
+    candidates=[]
+    candidates.append(rowOne[2])
+    votesRecieved=[1]
     for row in csvreader:
-        voteN += 1
-        #print(row)
+        #if voteN < 10:
+            voteN += 1
+            #print(row)
+            counter= 0
+            for candidate in candidates:
+                counter += 1
+                if candidate == row[2]:
+                    i= candidates.index(candidate)
+                    votesRecieved[i] += 1
+                    break
+                elif counter == len(candidates):
+                    #print("Counter "+str(counter))
+                    #print("Len"+str(len(candidates)))
+                    candidates.append(row[2])
+                    votesRecieved.append(0)
+                
+        #print(str(voteN))
+        
     
     print("Election Results")
     print("-------------------------")
     print(f"Total Votes: {voteN}")
     print("-------------------------")
+    winnerValue=1 
+    for i in range(len(candidates)):
+        print(f"{candidates[i]} : {round((votesRecieved[i]/ voteN)*100, 3)} % ({votesRecieved[i]})")
+        if winnerValue < round((votesRecieved[i]/ voteN)*100,3):
+            winnerValue = round((votesRecieved[i]/ voteN)*100,3)
+            winnerName = candidates[i]
     print("-------------------------")
+    print(f"Winner : {winnerName}")
     print("-------------------------")
+    print()
     
